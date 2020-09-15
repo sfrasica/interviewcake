48ff3ca5ea42c4df890587f979de5aeef900e116
Steven Frasica <sfrasica1@gmail.com>
2020-09-15 14:24:13 -0400

    steps recursive soln

--- steps.js
+++ steps.js
@@ -16,19 +16,23 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    for (let row = 0; row < n; row++) {
-        let stair = '';
-        for (let column = 0; column < n; column++) {
-            if (column <= row) {
-                stair += "#"
-            } else {
-                stair += " "
-            }
-        }
-        console.log(stair)
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
     }
-}
+  
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+  
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair)
+  }
 
 module.exports = steps;
 
@@ -47,6 +51,13 @@ module.exports = steps;
 
         In the first for loop
 
+
+
+*/
+
+/*
+Recursive solution
+Establish a base case
 
 
 */
