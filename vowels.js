db497b34bdc79b2f5cc6c10be5bfe9f2f819e684
Steven Frasica <sfrasica1@gmail.com>
2020-09-16 11:30:50 -0400

    vowels iterative soln

--- vowels.js
+++ vowels.js
@@ -2,3 +2,18 @@ function vowels(str) {
     const matches = str.match(/[aeiou]/gi);
     return matches ? matches.length : 0;
 }
+
+//Iterative solution
+
+function vowels(str) {
+    let count = 0;
+    const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+
+}
