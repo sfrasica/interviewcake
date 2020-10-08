bfed26ab6ea8d1da82b2394611d69426b591219a
Steven Frasica <sfrasica1@gmail.com>
2020-10-07 20:28:29 -0400

    traversal breadth first

--- tree.js
+++ tree.js
@@ -16,6 +16,20 @@ class Node {
 
 }
 
-class Tree {}
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root];
+
+        while (arr.length) {
+            const node = arr.shift();
+            arr.push(...node.children);
+            fn(node);
+        }
+    }
+}
 
 module.exports = { Tree, Node };
