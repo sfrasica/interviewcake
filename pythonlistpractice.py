5658d1745246c664810bfc7ba19d829dc32deec2
Steven Frasica <sfrasica1@gmail.com>
2020-11-10 15:29:00 -0500

    python for loop practice

--- pythonlistpractice.py
+++ pythonlistpractice.py
@@ -1,2 +1,11 @@
 def remove_middle(lst, start, end):
   return lst[:start] + lst[end+1:]
+
+dog_breeds_available_for_adoption = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+dog_breed_I_want = 'dalmatian'
+
+for dog in dog_breeds_available_for_adoption:
+  print(dog)
+  if dog == dog_breed_I_want:
+    print("They have the dog I want!")
+    break
