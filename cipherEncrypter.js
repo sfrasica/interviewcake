42d00dc3ec9854f4218ef5c88ad6a4876945d97d
Steven Frasica <sfrasica1@gmail.com>
2020-11-22 14:41:11 -0500

    cypher encrypter practice

---
+++ cipherEncrypter.js
@@ -0,0 +1,12 @@
+function caesarCipherEncryptor(string, key) {
+    // Write your code here.
+      const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+      let strArr = string.split('')
+      let shiftedArr = [];
+      for (let i = 0; i < strArr.length; i++) {
+           let lettersIndex = letters.indexOf(strArr[i])
+           if (lettersIndex + key > 25) {
+               lettersIndex = (lettersIndex + key) % 26;
+           }
+        }
+    }
